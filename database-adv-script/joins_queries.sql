000ee84746f2abc344f286ebbc8e452a59c4c627
M-Wonder <mybwndr17@gmail.com>
2025-09-07 22:40:15 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,15 @@
 
+
+
+/*left join*/
+SELECT 
+    p.property_id,
+    p.title,
+    p.location,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.review_date
+FROM properties p
+LEFT JOIN reviews r 
+    ON p.property_id = r.property_id;
